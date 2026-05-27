a15d9708414cfebb9912cdd92e49b74e1dcc1acb
Afonso de Mori <me@afonso.dev>
2026-05-27 15:04:46 +0000

    chore: Setup gotmplfmt

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -4,3 +4,5 @@ set -e
 # fns-cli
 sudo ln -sf $HOME/.fns-cli/container.bash_profile $HOME/fns-cli.bash_profile
 grep -qxF 'source ~/fns-cli.bash_profile' "$HOME/.bashrc" || printf '\nsource ~/fns-cli.bash_profile\n' >>"$HOME/.bashrc"
+
+go install github.com/gohugoio/gotmplfmt@latest
