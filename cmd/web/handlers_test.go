686232c958a1a4ddec3720149b700f6818b04d3f
Afonso de Mori <me@afonso.dev>
2026-06-02 16:06:59 +0000

    feat: 13.4 End-to-end testing

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io"
+	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,7 +11,7 @@ import (
 	"afonso.dev/snippetbox/internal/assert"
 )
 
-func TestPing(t *testing.T) {
+func TestPingV1(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	req, err := http.NewRequest(http.MethodGet, "/", nil)
@@ -32,4 +33,45 @@ func TestPing(t *testing.T) {
 	body = bytes.TrimSpace(body)
 
 	assert.Equal(t, string(body), "OK")
+}
+
+func TestPingV2(t *testing.T) {
+	app := &application{
+		logger: slog.New(slog.DiscardHandler),
+	}
+
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+
+	req, err := http.NewRequest(http.MethodGet, ts.URL+"/ping", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := ts.Client().Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	assert.Equal(t, res.StatusCode, http.StatusOK)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+}
+
+func TestPing(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	res := ts.get(t, "/ping")
+	assert.Equal(t, res.status, http.StatusOK)
+	assert.Equal(t, res.body, "OK")
 }
